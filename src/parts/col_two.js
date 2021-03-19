0138b32121f9e7cc19a4cced63b1445813af3e63
Matthew Skillman <mskillman@trek10.com>
2021-03-19 15:15:56 -0400

    day 3 update

--- src/parts/col_two.js
+++ src/parts/col_two.js
@@ -17,6 +17,12 @@ export const col_two = () => {
             (March 10 2021) The deposits finally arrived in my account. Started earning interest about a day ago;
             so far I only have 2 cents of interest earned.
         </p>
-        <img style={imageStyle} src="https://resources-linus23232.s3.amazonaws.com/55514.jpg" alt="day 1 progress"/>
+        <img style={imageStyle} src="https://resources-linus23232.s3.amazonaws.com/55514.jpg" alt="day 2 progress"/>
+        <p>
+            (March 19 2021) Linus is legit. I have started earning more interest. Also I tested the site by withdrawing some
+            money and the $10 I withdrew actually arrived in my bank account so I know that the site is not a scam now.
+        </p>
+        <img style={imageStyle} src="https://resources-linus23232.s3.amazonaws.com/day_three.jpg" alt="day 3 progress"/>
+        <img style={imageStyle} src="https://resources-linus23232.s3.amazonaws.com/day_four.jpg" alt="day 3 progress"/>
     </div>
 }
